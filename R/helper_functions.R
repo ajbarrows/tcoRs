7a778d1b6596b8d30f993132a8e3ce225eb9ba4a
Anthony Barrows <ajbarrow@uvm.edu>
2022-06-02 12:15:09 -0400

    Add matrix_to_var()

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -44,3 +44,52 @@ pi_prop <- function(df) {
   df
 }
 
+#' Convert matrix data to character vector
+#'
+#' @param df Data frame containing (not necessarily exclusively) matrix data.
+#' @param colname String of resulting column name
+#' @param matrix_vars Named list of matrix variables.
+#' @param select_string String to uniquely identify matrix variables to remove using dplyr::contains()
+#'
+#' @return Data frame with vectorized matrix variables.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' df <- matrix_to_vector(df, "new_col", matrix_list, "matrix_string")
+#' }
+matrix_to_vector <- function(df, colname, matrix_vars, select_string) {
+
+  rename_matrix <- function(df) {
+    # make values names of columns
+    w1 <- which(df[,1:ncol(df)] == 1, arr.ind = TRUE)
+    w0 <- which(df[,1:ncol(df)] == 0, arr.ind = TRUE)
+
+    mode(w1) <- "numeric"
+    mode(w0) <- "numeric"
+    # avoid zero-length error
+    if(length(w1 > 0)) {
+      df[w1] <- names(df)[w1[,"col"]]
+    }
+    df[w0] <- NA
+
+    df
+  }
+
+  df_sub <- df %>%
+    plyr::rename(matrix_vars) %>%
+    dplyr::select(unlist(matrix_vars, use.names = FALSE)) %>%
+    rename_matrix() %>%
+    tidyr::unite(
+      col = !!colname,
+      sep = ",",
+      na.rm = TRUE
+    )
+  df_sub[df_sub == ""] <- NA
+
+  df %>%
+    dplyr::select(-dplyr::contains(select_string)) %>%
+    cbind(df_sub)
+}
+
+
