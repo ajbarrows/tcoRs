14643bed3eb80c01d1eca9172a6255b6f4307b37
Anthony Barrows <ajbarrow@uvm.edu>
2022-06-13 16:41:57 -0400

    Add CTCAE crosswalk data set

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' NIH National Cancer Institute Common Terminology Criteria for Adverse Events (CTCAE)
+#'
+#' A crosswalk data set containing common terminologies for adverse events and
+#'
+#' @source \url{https://ctep.cancer.gov/protocoldevelopment/electronic_applications/ctc.htm#ctc_60}
+"ctcae5"
