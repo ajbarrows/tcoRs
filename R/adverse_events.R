37e894dad05e6d553bd48bd8544617d637827aaf
Anthony Barrows <ajbarrow@uvm.edu>
2022-06-15 13:49:17 -0400

    Add clean_ae()

--- R/adverse_events.R
+++ R/adverse_events.R
@@ -25,37 +25,54 @@ get_maintrial_aes <- function(rcon, fields = field_vectors$ae_fields) {
       )
     )
 }
-#
-#
-#
-#
-#
-# clean_ae <- function(ae_df) {
-#
-#   meddra_crosswalk <-
-#     readxl::read_excel("../data/CTCAE_v5.0.xlsx") %>%
-#     select(`MedDRA Code`, `MedDRA SOC`, `CTCAE Term`)
-#
-#   hardcode_list <- c(
-#     "10012378",
-#     "10002855",
-#     "10018065"
-#   )
-#
-#   ae_df %>%
-#
-#     mutate(ae_meddra = ifelse(ae_meddra1 == 0, ae_meddra2, ae_meddra1)) %>%
-#     mutate(
-#       ae_descrip = ifelse(ae_meddra == 10012378, "BDI Category Increase", ae_descrip),
-#       ae_descrip = ifelse(ae_meddra == 10002855, "OASIS Score Increase", ae_descrip),
-#       ae_descrip = ifelse(ae_meddra == 10018065, "CPD Increase", ae_descrip)
-#     ) %>%
-#     left_join(meddra_crosswalk, by = c("ae_meddra" = "MedDRA Code")) %>%
-#     mutate(ae_descrip = ifelse(
-#       stringr::str_detect(tolower(ae_descrip), "bdi|oasis|cpd"),
-#       ae_descrip,
-#       `CTCAE Term`
-#     )
-#     ) %>%
-#     filter(!is.na(ae_descrip))
-# }
+
+#' Clean Main Trial Adverse Events
+#'
+#' @param ae_df Adverse Event data frame exported from tcoRs::get_maintrial_aes()
+#'
+#' @return data frame of clean adverse events
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' rcon <- build_rcon("my_secret_token")
+#' ae_df <- get_maintrial_aes(rcon)
+#' clean_ae_df <- clean_ae(ae_df)
+#' }
+clean_ae <- function(ae_df) {
+  hardcode_list <- c(
+    "10012378",
+    "10002855",
+    "10018065"
+  )
+  names_list <- c(
+    "BDI Category Increase",
+    "OASIS Score Increase",
+    "CPD Increase"
+  )
+  join_list <- c("ae_meddra" = "MedDRA.Code")
+  det_string <- "bdi|oasis|cpd"
+
+  filter_select <- "redcap"
+
+  meddra <- tcoRs::ctcae5 %>%
+    dplyr::select(.data$MedDRA.Code, .data$CTCAE.Term)
+
+  ae_df %>%
+    dplyr::mutate(ae_meddra = ifelse(.data$ae_meddra1 == 0, .data$ae_meddra2, .data$ae_meddra1)) %>%
+    dplyr::mutate(
+      ae_descrip = ifelse(.data$ae_meddra == hardcode_list[1], names_list[1], .data$ae_descrip),
+      ae_descrip = ifelse(.data$ae_meddra == hardcode_list[2], names_list[2], .data$ae_descrip),
+      ae_descrip = ifelse(.data$ae_meddra == hardcode_list[3], names_list[3], .data$ae_descrip)
+    ) %>%
+    dplyr::left_join(meddra, by = join_list) %>%
+    dplyr::mutate(description = ifelse(
+      stringr::str_detect(tolower(.data$ae_descrip), det_string),
+      .data$ae_descrip,
+      .data$CTCAE.Term
+      )
+    ) %>%
+    dplyr::select(!tidyselect::starts_with(filter_select)) %>%
+    dplyr::select(-c(.data$ae_common)) %>%
+    dplyr::select(.data$screen_id, .data$description, .data$ae_descrip, .data$CTCAE.Term, tidyselect::everything())
+}
