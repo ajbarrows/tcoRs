310314fbc5adf559b0f9eb35c03f94d350d90c84
Anthony Barrows <ajbarrow@uvm.edu>
2022-06-21 10:23:38 -0400

    Build AE summary functions

--- R/adverse_events.R
+++ R/adverse_events.R
@@ -77,3 +77,84 @@ clean_ae <- function(ae_df) {
     dplyr::select(.data$screen_id, .data$description, .data$ae_descrip, .data$CTCAE.Term, tidyselect::everything())
 
 }
+
+summarize_ae <- function(ae_clean, cond, filter_proper = TRUE) {
+  if (filter_proper) {
+    ae_clean <- ae_clean %>%
+      pi_prop() %>%
+      dplyr::filter(.data$pi_prop == "proper")
+  }
+
+  ae_clean <- ae_clean %>%
+    dplyr::left_join(cond, by = "screen_id") %>%
+    dplyr::filter(!is.na(.data$ae_descrip))
+
+  review_set <- ae_clean %>%
+    dplyr::select(
+      .data$screen_id,
+      .data$description,
+      .data$ae_descrip,
+      .data$ae_meddra
+      )
+
+  list(ae_clean, review_set)
+}
+
+
+
+
+# count distinct subject-variable combinations
+# then count distinct subject-description-variable combinations
+
+count_ae <- function(ae_clean, var, by_condition = FALSE) {
+
+  if (by_condition) {
+    sub_var <- ae_clean %>%
+      dplyr::distinct(.data$screen_id, .data$trt_grp, !!as.name(var)) %>%
+      dplyr::group_by(.data$trt_grp, !!as.name(var)) %>%
+      dplyr::count() %>%
+      dplyr::filter(!is.na(!!as.name(var))) %>%
+      tidyr::pivot_wider(
+        names_from = c(.data$trt_grp,!!as.name(var)),
+        values_from = "n",
+        values_fill = 0
+      )
+
+    sub_descrip_var <- ae_clean %>%
+      dplyr::distinct(.data$screen_id, .data$trt_grp, .data$description, !!as.name(var)) %>%
+      dplyr::group_by(.data$description, .data$trt_grp, !!as.name(var)) %>%
+      dplyr::count() %>%
+      tidyr::drop_na() %>%
+      tidyr::pivot_wider(
+        names_from = c(.data$trt_grp,!!as.name(var)),
+        values_from = "n",
+        values_fill = 0
+      )
+
+  } else{
+    sub_var <- ae_clean %>%
+      dplyr::distinct(.data$screen_id, !!as.name(var)) %>%
+      dplyr::group_by(!!as.name(var)) %>%
+      dplyr::count() %>%
+      dplyr::filter(!is.na(!!as.name(var))) %>%
+      tidyr::pivot_wider(
+        names_from = !!as.name(var),
+        values_from = "n",
+        values_fill = 0
+      )
+
+    sub_descrip_var <- ae_clean %>%
+      dplyr::distinct(.data$screen_id, .data$description, !!as.name(var)) %>%
+      dplyr::group_by(.data$description, !!as.name(var)) %>%
+      dplyr::count() %>%
+      tidyr::drop_na() %>%
+      tidyr::pivot_wider(
+        names_from = !!as.name(var),
+        values_from = "n",
+        values_fill = 0
+      )
+  }
+
+  return(list(sub_var, sub_descrip_var))
+}
+
